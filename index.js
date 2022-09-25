135c8655095dccbe01a93cb641e79d500586f395
SR-Shahen <s.r.shahenda@gmail.com>
2022-09-25 20:36:45 +0600

    added products with mvc pattern

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
 const express = require("express");
+const { products } = require("./models/products.models");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const productRouter = require("./routers/products.router");
 app.use(express.urlencoded({ extended: true }));
-
+app.use(productRouter);
 app.get('/', (req, res) => {
     res.send('Mvc Architecture server is running')
 })
-const products = [
-    {
-        name: "rice",
-        price: '35'
-    },
-    {
-        name: "biryani",
-        price: '54'
-    }
-]
-app.get('/products', (req, res) => {
-    res.send(htmlFrom)
-})
 
-const htmlFrom = `
-<form method="POST" action="/products" >
-    <input type="text" name="name" placeholder="Enter name"/>
-    <input type="number" name="price" placeholder="Enter price"/>
-    <button type="submit">Save Products</button>
-</form>
-`
-app.post('/products', (req, res) => {
-    const name = req.body.name;
-    const price = Number(req.body.price);
-    const product = {
-        name,
-        price
-    }
-    products.push(product);
-    res.status(201).json({
-        success: true,
-
-        products,
-    });
-});
 app.use((req, res, next) => {
     res.status(400).json({
         message: "Page not found"
---
+++ models/products.models.js
@@ -0,0 +1,11 @@
+const products = [
+    {
+        name: "rice",
+        price: '35'
+    },
+    {
+        name: "biryani",
+        price: '54'
+    }
+]
+module.exports = products;
